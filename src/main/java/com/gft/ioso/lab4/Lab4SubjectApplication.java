496c236b2cbc4f6ebaf1240ea56efacf3dcf4cd5
Ignacio Serrano Zabaleta <ignacio.serrano@gft.com>
2017-05-01 11:49:21 +0200

    Project import.

---
+++ src/main/java/com/gft/ioso/lab4/Lab4SubjectApplication.java
@@ -0,0 +1,12 @@
+package com.gft.ioso.lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4SubjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4SubjectApplication.class, args);
+	}
+}
